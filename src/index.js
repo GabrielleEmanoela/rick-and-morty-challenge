0a405cac4b51e101450bbe2f8fcfc7b6b6489358
Gabrielle Oliveira <biela.a09@gmail.com>
2021-04-30 16:17:54 -0300

    Configurando reactotron.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
-import { StatusBar } from 'react-native'
-import Routes from './routes'
+import React from 'react';
+import { StatusBar } from 'react-native';
+import Routes from './routes';
+import './config/Reactotron';
 
 export default function App() {
-  return <>
-    <StatusBar barStyle="light-content" backgroundColor="#00BCD4" />
-    <Routes />
-  </>
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#00BCD4" />
+      <Routes />
+    </>
+  );
 }
