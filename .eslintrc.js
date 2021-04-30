fe98bed2b95d2dc5ac93b08d13eee017a277e1be
Gabrielle Oliveira <biela.a09@gmail.com>
2021-04-30 00:27:03 -0300

    Configurando eslintrc é babel plugin.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,52 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+    jest: true,
+    browser: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'jsf-ally',"import","react-hooks","prettier"],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    "no-unused-vars": ["error",{extensions:[".js",]}],
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    "global-require": "off",
+    'react-native/no-raw-text': "off",
+    "react/jsx-one-expression-per-line": 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'no-param-reassign': 'off',
+    camelcase:"off",
+    'no-console': ["error", {allow :[ "tron"]}],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+  settigns: {
+    "import/resolver":{
+      "babel-plugin-root-import":{
+        rootPathSuffix: "src"
+      }
+    }
+  }
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  puglins: [
+    [
+      'babel-plugin-root-import',
+      {
+        rootPathSuffix: 'src',
+      },
+    ],
+  ],
 };
